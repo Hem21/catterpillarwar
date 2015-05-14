03044b5c5156b42b119e58baf12c3bb185a61267
Charles Gershom <charles.gershom@gmail.com>
2015-05-14 20:08:10 +0100

    Bad thing update - fixed now

--- CatterPillarWarUnityProject/Assets/dudeControl.cs
+++ CatterPillarWarUnityProject/Assets/dudeControl.cs
@@ -20,25 +20,17 @@ public class dudeControl : MonoBehaviour {
 	public int movementDelayMax=10;
 	public int turnSpeed=10;
 
-	public string Controllerx;
-	public string Controllery;
-	public string ControllerForwards;
-	public string ControllerBackwards;
-	public string ControllerTilt;
-	public string ControllerJump;
-	public string ControllerBump;
-
 	void Start () {
 	
 	}
 
 	// Update is called once per frame
 	void Update () {
-		movementVector.x = Input.GetAxis(Controllerx) * turnSpeed;
-		movementVector.y = Input.GetAxis(Controllery) * movementSpeed;
+		movementVector.x = Input.GetAxis("Player1X") * turnSpeed;
+		movementVector.y = Input.GetAxis("Player1Y") * movementSpeed;
 
 
-		if (Input.GetButtonDown (ControllerJump)) {
+		if (Input.GetButtonDown ("Player1Jump")) {
 			Debug.Log("ControllerJump!");
 			for (int segs=12;segs>0;segs--){
 			segments[segs].GetComponent<Rigidbody>().velocity=segments[segmentCount].GetComponent<Rigidbody>().velocity + new Vector3(0,4,0);
@@ -47,60 +39,61 @@ public class dudeControl : MonoBehaviour {
 
 		}
 
-		float tiltX = Input.GetAxis (Controllerx) * 2000;
-		float tiltY = Input.GetAxis (Controllery) * 4000;
+		//float tiltX = Input.GetAxis (Controllerx) * 2000;
+		//float tiltY = Input.GetAxis (Controllery) * 4000;
 
 
 
 
-		if (Input.GetButtonDown (ControllerBump)) {
-			Debug.Log("ControllerBump!");
-			//segments[1].GetComponent<Rigidbody>().velocity=segments[segmentCount].GetComponent<Rigidbody>().velocity + new Vector3(0,-5,0);
-			//segments[1].GetComponent<Rigidbody>().AddRelativeForce(100000,0,0);
-			segments[12].GetComponent<Rigidbody>().AddRelativeForce(100000,100000,0);
-		}
-
-		if (Input.GetButton (ControllerForwards)) {
-			Debug.Log("Forwards!");
-			movementForwardBack = 1;
-			//movingObject.GetComponent<Rigidbody>().AddRelativeForce(movementSpeedTrim,-100,0);
-			anchorObject.GetComponent<Rigidbody>().AddRelativeForce(0,-100,0);
-			//steeringObject.GetComponent<Rigidbody>().AddRelativeForce(0,100,0);
-		}
-
-
-
-
-		if (Input.GetButton (ControllerBackwards)) {
-			Debug.Log("backwards!");
-			movementForwardBack = -1;
-			movingObject.GetComponent<Rigidbody>().AddRelativeForce(-movementSpeedTrim,-100,0);
-			anchorObject.GetComponent<Rigidbody>().AddRelativeForce(0,-100,0);
-		}
-
-		
-		if (Input.GetButtonUp (ControllerForwards)) {
-			Debug.Log("Stop!");
-			movementForwardBack = 0;
-		}
-		
-		if (Input.GetButtonUp (ControllerBackwards)) {
-			Debug.Log("Stop!");
-			movementForwardBack = 0;
-		}
-//		if (movementVector.y > 0) {
+//		if (Input.GetButtonDown (ControllerBump)) {
+//			Debug.Log("ControllerBump!");
+//			//segments[1].GetComponent<Rigidbody>().velocity=segments[segmentCount].GetComponent<Rigidbody>().velocity + new Vector3(0,-5,0);
+//			//segments[1].GetComponent<Rigidbody>().AddRelativeForce(100000,0,0);
+//			segments[12].GetComponent<Rigidbody>().AddRelativeForce(100000,100000,0);
+//		}
+//
+//		if (Input.GetButton (ControllerForwards)) {
+//			Debug.Log("Forwards!");
 //			movementForwardBack = 1;
 //			//movingObject.GetComponent<Rigidbody>().AddRelativeForce(movementSpeedTrim,-100,0);
 //			anchorObject.GetComponent<Rigidbody>().AddRelativeForce(0,-100,0);
 //			//steeringObject.GetComponent<Rigidbody>().AddRelativeForce(0,100,0);
 //		}
 //
-//		if (movementVector.y < 0) {
+//
+//
+//
+//		if (Input.GetButton (ControllerBackwards)) {
+//			Debug.Log("backwards!");
 //			movementForwardBack = -1;
 //			movingObject.GetComponent<Rigidbody>().AddRelativeForce(-movementSpeedTrim,-100,0);
 //			anchorObject.GetComponent<Rigidbody>().AddRelativeForce(0,-100,0);
-//			//steeringObject.GetComponent<Rigidbody>().AddRelativeForce(0,100,0);
 //		}
+
+		
+		if (Input.GetButtonUp ("Player1X")) {
+			Debug.Log("Stop!");
+			movementForwardBack = 0;
+		}
+//		
+//		if (Input.GetButtonUp (ControllerBackwards)) {
+//			Debug.Log("Stop!");
+//			movementForwardBack = 0;
+//		}
+
+		if (movementVector.y < 0) {
+			movementForwardBack = 1;
+			//movingObject.GetComponent<Rigidbody>().AddRelativeForce(movementSpeedTrim,-100,0);
+			anchorObject.GetComponent<Rigidbody>().AddRelativeForce(0,-100,0);
+			//steeringObject.GetComponent<Rigidbody>().AddRelativeForce(0,100,0);
+		}
+
+		if (movementVector.y > 0) {
+			movementForwardBack = -1;
+			movingObject.GetComponent<Rigidbody>().AddRelativeForce(-movementSpeedTrim,-100,0);
+			anchorObject.GetComponent<Rigidbody>().AddRelativeForce(0,-100,0);
+			//steeringObject.GetComponent<Rigidbody>().AddRelativeForce(0,100,0);
+		}
 
 		if (movementVector.y ==0) {
 			segmentCount=segmantMax;
